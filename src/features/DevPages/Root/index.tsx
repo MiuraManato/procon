3896b7f658015ec66807e1ab35c0ffd8c465e9dd
MiuraManato <manato20030510@icloud.com>
2023-11-07 00:14:56 +0900

    feat: Add links and styles to developer page

--- src/features/DevPages/Root/index.tsx
+++ src/features/DevPages/Root/index.tsx
@@ -7,18 +7,18 @@ export const RootPage = () => {
 			<h1 className={styles.title}>開発者用ページ *開発終了時に削除します</h1>
 			<div className={styles.list}>
 				<div className={styles.items}>
-					<Link href={"/employees"}>
+					<Link href={"/employees"} className={styles.link}>
 						<div className={styles.item}>従業員用</div>
 					</Link>
 				</div>
 				<div className={styles.items}>
-					<Link href={"/order"}>
+					<Link href={"/order/menu"} className={styles.link}>
 						<div className={styles.item}>注文端末用</div>
 					</Link>
 				</div>
 				<div className={styles.items}>
-					<Link href={"/User"}>
-						<div className={styles.item}>Webシステム用</div>
+					<Link href={"/user/top"} className={styles.link}>
+						<div className={styles.item}>Webシステム用(ユーザー)</div>
 					</Link>
 				</div>
 			</div>
