8f6e18e35ff03bf65e0fd4ddd61ec646d5edcb6c
manato miura <mmiura2351@gmail.com>
2023-11-10 11:23:16 +0900

    feat: サインアップのAPIエンドポイントを作成

---
+++ src/pages/api/auth/signup/index.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiResponse } from "next";
+import { NextApiRequestExtendsUser } from "./type";
+
+const signupHandler = async (req: NextApiRequestExtendsUser, res: NextApiResponse) => {
+  const prisma = new PrismaClient();
+  const { uid, username, firstName, lastName, age, email } = req.body;
+  if (!uid || !username || !firstName || !lastName || !email) {
+    return res.status(400).json({ message: "Invalid request" });
+  }
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        userId: uid,
+        username: username,
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+        email: email,
+      },
+    });
+    res.status(200).json({ message: "User created successfully", user });
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong. Please try again later." });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export default signupHandler;
