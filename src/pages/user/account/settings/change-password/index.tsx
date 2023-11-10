623ad7b3de6019262c8ccf4e6803fa08480aa4b2
ryoki <mfjzc4xh2@gmail.com>
2023-11-10 14:05:26 +0900

    feat: パスワード変更画面の呼び出し部分の作成

---
+++ src/pages/user/account/settings/change-password/index.tsx
@@ -0,0 +1,9 @@
+//パスワード変更画面
+//制作者：吉澤
+import { ChangePassword } from "@/features/User/AccountSettings/ChangePassword/"
+
+const ChangePasswordPage = () => {
+  return <ChangePassword />;
+};
+
+export default ChangePasswordPage;
