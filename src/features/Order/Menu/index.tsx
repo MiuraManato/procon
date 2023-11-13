0d8b40feb01faee46c3dab032ef9a83b9b0e45a0
MiuraManato <manato20030510@icloud.com>
2023-11-13 23:11:05 +0900

    feat: Add CategoryMenu component to display menu data

---
+++ src/features/Order/Menu/index.tsx
@@ -0,0 +1,6 @@
+import { MenuData } from "@/features/Order/Menu/type";
+
+export const CategoryMenu = ({ menuData }: { menuData: MenuData }) => {
+  console.log(menuData);
+  return <div>CategoryMenu</div>;
+};
