b15b89c77d9758557bd082c08d249fd44d8352db
ryoki <mfjzc4xh2@gmail.com>
2023-11-14 14:44:46 +0900

    feat: パスワード変更の処理部分にログイン処理を追加

--- src/features/User/AccountSettings/ChangePassword/doChangePassword.ts
+++ src/features/User/AccountSettings/ChangePassword/doChangePassword.ts
@@ -1,13 +1,17 @@
 import { FirebaseError } from "firebase/app";
-import { updatePassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, updatePassword } from "firebase/auth";
 import useAuth from "@/features/hooks/useAuth";
+import { firebaseApp } from "@/utils/Firebase/firebaseConfig";
 
-export const DoChangePassword = async (newPassword: string): Promise<boolean> => {
+export const DoChangePassword = async (oldPassword: string, newPassword: string): Promise<boolean> => {
+  const user = useAuth();
   try {
-    const user = useAuth();
-    if (user === null) {
+    const auth = getAuth(firebaseApp);
+    if (user === null || user.email === null) {
       return false;
     }
+    const email = user.email;
+    await signInWithEmailAndPassword(auth, email, oldPassword);
     await updatePassword(user, newPassword);
     return true;
   } catch (e) {
