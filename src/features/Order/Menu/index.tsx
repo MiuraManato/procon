4967cc8dad57cb69d8c8caf832cb4fb26ddf0025
manato miura <mmiura2351@gmail.com>
2023-11-14 11:09:31 +0900

    feat: Add menu category list to CategoryMenu component.

--- src/features/Order/Menu/index.tsx
+++ src/features/Order/Menu/index.tsx
@@ -1,6 +1,17 @@
 import { MenuData } from "@/features/Order/Menu/type";
+import styles from "./index.module.css";
 
 export const CategoryMenu = ({ menuData }: { menuData: MenuData }) => {
   console.log(menuData);
-  return <div>CategoryMenu</div>;
+  return (
+    <>
+      <div className={`${styles["menu-container"]}`}>
+        {menuData.map((menu) => (
+          <div key={menu.menuCategoryName} className={`${styles["category-container"]}`}>
+            <p className={`${styles["category-list"]}`}>{menu.menuCategoryName}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
