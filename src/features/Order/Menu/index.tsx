8a2f32433df77f31d9d68eb4fc2106ea17205b71
manato miura <mmiura2351@gmail.com>
2023-11-14 13:35:14 +0900

    feat: 商品名の表示を作成

--- src/features/Order/Menu/index.tsx
+++ src/features/Order/Menu/index.tsx
@@ -1,16 +1,37 @@
 import { MenuData } from "@/features/Order/Menu/type";
 import styles from "./index.module.css";
+import { useState } from "react";
 
 export const CategoryMenu = ({ menuData }: { menuData: MenuData }) => {
+  const [nowCategory, setNowCategory] = useState<string | null>(null);
   console.log(menuData);
   return (
     <>
       <div className={`${styles["menu-container"]}`}>
         {menuData.map((menu) => (
-          <div key={menu.menuCategoryName} className={`${styles["category-container"]}`}>
-            <p className={`${styles["category-list"]}`}>{menu.menuCategoryName}</p>
-          </div>
+          <>
+            <div key={menu.menuCategoryName} className={`${styles["category-container"]}`}>
+              <button className={`${styles["category-button"]}`} onClick={() => setNowCategory(menu.menuCategoryName)}>
+                <p className={`${styles["category-list"]}`}>{menu.menuCategoryName}</p>
+              </button>
+            </div>
+          </>
         ))}
+      </div>
+      <div>
+        {menuData.map(
+          (menu) =>
+            nowCategory === menu.menuCategoryName && (
+              <div key={menu.menuId} className={styles["menu-list"]}>
+                {menu.menuProducts.map((product) => (
+                  <div key={product.menuProductId} className={styles["product-item"]}>
+                    <p>{product.product.productName}</p>
+                    <p>{product.product.price}</p>
+                  </div>
+                ))}
+              </div>
+            ),
+        )}
       </div>
     </>
   );
