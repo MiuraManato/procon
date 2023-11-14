5bab3dc0952c170614913b88cd496d32231dfdda
manato miura <mmiura2351@gmail.com>
2023-11-14 11:13:04 +0900

    fix: ユーザー取得時にfirebaseのインスタンスが作成されていない場合に発生するエラーを修正

--- src/features/hooks/useAuth/index.ts
+++ src/features/hooks/useAuth/index.ts
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
+import { getApps, initializeApp } from "firebase/app";
 import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import { firebaseConfig } from "@/utils/Firebase/firebaseConfig";
 
 const useAuth = () => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
+    if (getApps().length) {
+      initializeApp(firebaseConfig);
+    }
     const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
