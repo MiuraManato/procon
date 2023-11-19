f234709099498f9de76d722d599b8ba6414125f5
MiuraManato <manato20030510@icloud.com>
2023-11-19 18:57:13 +0900

    feat: Add getAllergies and getIngredients functions for AddProductPage

--- src/pages/employee/menu/add/index.tsx
+++ src/pages/employee/menu/add/index.tsx
@@ -1,7 +1,22 @@
 import { AddProduct } from "@/features/Employee/Product/Add";
+import { getAllergies } from "@/features/Employee/Product/Add/getAllergies";
+import { getIngredients } from "@/features/Employee/Product/Add/getIngredients";
+import { Allergy, Ingredient } from "@prisma/client";
 
-const AddProductPage = () => {
-  return <AddProduct />;
+const AddProductPage = ({ allergies, ingredients }: { allergies: Allergy[]; ingredients: Ingredient[] }) => {
+  return <AddProduct allergies={allergies} ingredients={ingredients} />;
+};
+
+export const getServerSideProps = async () => {
+  const allergies = await getAllergies();
+  const ingredients = await getIngredients();
+
+  return {
+    props: {
+      allergies,
+      ingredients,
+    },
+  };
 };
 
 export default AddProductPage;
