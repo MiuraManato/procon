8a23e34620820fc6820e97707f2db1f5672e858a
MiuraManato <manato20030510@icloud.com>
2023-11-19 10:39:28 +0900

    feat: Add getAllergies functionality to MenuPage

--- src/pages/order/menu/index.tsx
+++ src/pages/order/menu/index.tsx
@@ -1,17 +1,21 @@
 import { CategoryMenu } from "@/features/Order/Menu";
 import { getMenuData } from "@/features/Order/Menu/getMenu";
 import { MenuData } from "@/features/Order/Menu/type";
+import { getAllergies } from "@/features/Order/Menu/getAllergies";
+import { Allergy } from "@prisma/client";
 
-const MenuPage = ({ menuData }: { menuData: MenuData }) => {
-  return <CategoryMenu menuData={menuData} />;
+const MenuPage = ({ menuData, allergies }: { menuData: MenuData, allergies: Allergy[] }) => {
+  return <CategoryMenu menuData={menuData} allergies={allergies} />;
 };
 
 export const getServerSideProps = async () => {
   const menuData = await getMenuData();
+  const allergies = await getAllergies();
 
   return {
     props: {
       menuData,
+      allergies,
     },
   };
 };
