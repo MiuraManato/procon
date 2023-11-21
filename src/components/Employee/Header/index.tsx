5db6fe81277e757fd49e3e1e20137c96590bfa95
MiuraManato <manato20030510@icloud.com>
2023-11-22 00:04:02 +0900

    feat: Add Employee Header component

--- src/components/Employee/Header/index.tsx
+++ src/components/Employee/Header/index.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+import styles from "./index.module.css";
+
 export const EHeader = () => {
   return (
-    <div>
-      <h1>Employee Header</h1>
-    </div>
+    <>
+      <div className={styles["header-container"]}>
+        <div className={styles["header-left"]}>
+          <div className={styles["header-item"]}>
+            <Link href="/employee/top">トップ</Link>
+          </div>
+          <div className={styles["header-item"]}>
+            <Link href="/employee/order-list">オーダー一覧</Link>
+          </div>
+          <div className={styles["header-item"]}>
+            <Link href="/employee/table/edit">席編集</Link>
+          </div>
+          <div className={styles["header-item"]}>
+            <Link href="/employee/menu/settings">メニュー設定</Link>
+          </div>
+        </div>
+        <div className={styles["header-right"]}>
+          <div className={styles["header-item"]}>
+            <p>ログアウト</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
