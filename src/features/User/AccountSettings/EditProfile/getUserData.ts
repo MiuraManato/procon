76b29e806bf34587821fac0c89957047ee33f49b
yuto <yuto154110@gmail.com>
2023-11-21 11:32:40 +0900

    getUserData関数の修正

--- src/features/User/AccountSettings/EditProfile/getUserData.ts
+++ src/features/User/AccountSettings/EditProfile/getUserData.ts
@@ -2,13 +2,14 @@ import { prisma } from "@/utils/Prisma/PrismaClient";
 
 export const getUserData = async (userId: string) => {
   try {
-    const user = await prisma.user.findMany({
+    const user = await prisma.user.findUnique({
       where: {
-        userId,
+        userId:userId,
       },
     });
     return user;
   } catch (error) {
+    console.error(error);
     throw new Error("Failed to fetch user data");
   }
 };
