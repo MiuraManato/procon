624f3e36a2d0c931f37ddbb196bfa3ba57516aeb
MiuraManato <manato20030510@icloud.com>
2023-11-24 18:34:22 +0900

    feat: Add TableDetail component and TableDetailPage

---
+++ src/features/Employee/Table/Detail/index.tsx
@@ -0,0 +1,6 @@
+import { Tables } from "../../Top/type";
+
+export const TableDetail = ({ table }: { table: Tables }) => {
+  console.log(table);
+  return <div>TableDetail</div>;
+};
---
+++ src/pages/employee/table/detail/[tableId].tsx
@@ -0,0 +1,54 @@
+import { TableDetail } from "@/features/Employee/Table/Detail";
+import { getTable } from "@/features/Employee/Top/getTables";
+import { Tables } from "@/features/Employee/Top/type";
+import { GetServerSideProps } from "next";
+import { ParsedUrlQuery } from "querystring";
+
+const TableDetailPage = ({ table }: { table: Tables }) => {
+  return <TableDetail table={table} />;
+};
+
+const extractTableId = (params: ParsedUrlQuery): number | null => {
+  const tableId = params.tableId;
+  if (typeof tableId === "string") {
+    const parsedId = parseInt(tableId, 10);
+    return isNaN(parsedId) ? null : parsedId;
+  }
+  return null;
+};
+
+const fetchTableData = async (tableId: number) => {
+  try {
+    const table = await getTable(tableId);
+    return table;
+  } catch (error) {
+    console.error("Failed to fetch table data:", error);
+    return null;
+  }
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tableId = context.params ? extractTableId(context.params) : null;
+
+  if (tableId === null) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const table = await fetchTableData(tableId);
+
+  if (!table) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      table,
+    },
+  };
+};
+
+export default TableDetailPage;
