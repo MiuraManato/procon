78e6b1def5688bddc6a6c495421994c545ee5d3a
MiuraManato <manato20030510@icloud.com>
2023-11-26 22:45:06 +0900

    feat: モーダルを作成した

--- src/features/Employee/OrderList/index.tsx
+++ src/features/Employee/OrderList/index.tsx
@@ -1,9 +1,34 @@
 import { Order } from "./type";
 import { formatTime } from "@/utils/Formatters/formatTime";
 import styles from "./index.module.css";
+import { useState } from "react";
 
 export const OrderList = ({ orders }: { orders: Order[] }) => {
-  console.log(orders);
+  // モーダルの表示状態を管理するstate
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // 選択されたオーダーを管理するstate
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+
+  // モーダルを開く関数
+  const handleOpenModal = (order: Order) => {
+    setSelectedOrder(order);
+    setIsModalOpen(true);
+  };
+
+  // モーダルを閉じる関数
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // モーダルの外側をクリックした時にモーダルを閉じる関数
+  const handleClickOutside = () => {
+    setIsModalOpen(false);
+  };
+
+  // モーダル内の要素をクリックした時にモーダルを閉じないようにする関数
+  const handleCLickModal = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
 
   return (
     <>
@@ -25,12 +50,24 @@ export const OrderList = ({ orders }: { orders: Order[] }) => {
                     </li>
                   ))}
                 </ul>
-                <button>変更</button>
+                <button className={styles["button"]} onClick={() => handleOpenModal(order)}>
+                  変更
+                </button>
               </div>
             ))}
           </div>
         </div>
       </div>
+      {isModalOpen && selectedOrder && (
+        <div className={styles.modal} onClick={handleClickOutside}>
+          <div className={styles.modalContent} onClick={handleCLickModal}>
+            <h2>{selectedOrder.storeTable.tableName}</h2>
+            <button className={styles["button"]} onClick={handleCloseModal}>
+              閉じる
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
