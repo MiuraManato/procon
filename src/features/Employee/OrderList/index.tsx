2d7203b2a181c580be684a902f2db8deebf9d8da
MiuraManato <manato20030510@icloud.com>
2023-11-26 21:58:24 +0900

    feat: オーダーの情報を表示させた

--- src/features/Employee/OrderList/index.tsx
+++ src/features/Employee/OrderList/index.tsx
@@ -1,12 +1,36 @@
 import { Order } from "./type";
+import { formatTime } from "@/utils/Formatters/formatTime";
+import styles from "./index.module.css";
 
 export const OrderList = ({ orders }: { orders: Order[] }) => {
   console.log(orders);
 
   return (
     <>
-      <h1>OrderList</h1>
-      <div>TODO</div>
+      <div className={styles["container"]}>
+        <h1>オーダー一覧</h1>
+        <div className={styles["order-list"]}>
+          <div className={styles["order-container"]}>
+            {orders.map((order) => (
+              <div key={order.orderId} className={styles["order-card"]}>
+                <div className={styles["order-header"]}>
+                  <h2>{order.storeTable.tableName}</h2>
+                  <p>{formatTime(order.orderedAt)}</p>
+                </div>
+                <ul className={styles["order-details"]}>
+                  {order.orderDetail.map((detail) => (
+                    <li key={detail.orderDetailId} className={styles["order-item"]}>
+                      <span>{detail.product.productName}</span>
+                      <span>×{detail.quantity}</span>
+                    </li>
+                  ))}
+                </ul>
+                <button>変更</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
