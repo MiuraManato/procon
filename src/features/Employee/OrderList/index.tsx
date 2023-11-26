debb1f5247ba2aad781d90ad97bce8f711704dce
MiuraManato <manato20030510@icloud.com>
2023-11-26 20:44:23 +0900

    feat: Add OrderList component and server-side rendering for order list page

---
+++ src/features/Employee/OrderList/index.tsx
@@ -0,0 +1,12 @@
+import { Order } from "./type";
+
+export const OrderList = ({ orders }: { orders: Order[] }) => {
+  console.log(orders);
+
+  return (
+    <>
+      <h1>OrderList</h1>
+      <div>TODO</div>
+    </>
+  );
+};
