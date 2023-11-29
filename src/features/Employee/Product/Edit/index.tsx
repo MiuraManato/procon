af7edc43576cfa9f267c438a4e44fac1fe05e03d
MiuraManato <manato20030510@icloud.com>
2023-11-29 23:17:52 +0900

    feat: ingredientを選択できるようにした

--- src/features/Employee/Product/Edit/index.tsx
+++ src/features/Employee/Product/Edit/index.tsx
@@ -1,9 +1,20 @@
-import { Category, Product } from "@prisma/client";
+import { Category, Ingredient, Product } from "@prisma/client";
 import { FormEvent, useState } from "react";
 import styles from "./index.module.css";
 
-export const ProductEdit = ({ product, categories }: { product: Product; categories: Category[] }) => {
+export const ProductEdit = ({
+  product,
+  categories,
+  ingredients,
+}: {
+  product: Product;
+  categories: Category[];
+  ingredients: Ingredient[];
+}) => {
   const [newProduct, setNewProduct] = useState<Product>(product);
+  const [openIngredientModal, setOpenIngredientModal] = useState<boolean>(false);
+  const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
+  const [ingredient, setIngredient] = useState<number[]>([]);
 
   function handleSubmit(event: FormEvent<HTMLFormElement>): void {
     event.preventDefault();
@@ -23,6 +34,49 @@ export const ProductEdit = ({ product, categories }: { product: Product; categor
 
   const handleSetCategory = (value: string): void => {
     setNewProduct({ ...newProduct, categoryId: parseInt(value, 10) });
+  };
+
+  const handleModalOutsideClick = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      setOpenIngredientModal(false);
+      // setOpenAllergyModal(false);
+    }
+  };
+
+  const handleModalInsideClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
+  const toggleIngredientSelection = (ingredientId: number) => {
+    setIngredient((prev) => {
+      const newSelection = new Set(prev);
+
+      if (newSelection.has(ingredientId)) {
+        newSelection.delete(ingredientId);
+      } else {
+        newSelection.add(ingredientId);
+      }
+
+      const newSelectedIngredients = ingredients
+        .filter((ingredient) => newSelection.has(ingredient.ingredientId))
+        .map((ingredient) => ingredient.ingredientName);
+
+      setSelectedIngredients(newSelectedIngredients);
+      return Array.from(newSelection);
+    });
+  };
+
+  const clearSelectedItems = (key: "allergy" | "ingredient") => {
+    switch (key) {
+      // case "allergy":
+      //   setAllergy([]);
+      //   setSelectedAllergies([]);
+      //   break;
+      case "ingredient":
+        setIngredient([]);
+        setSelectedIngredients([]);
+        break;
+    }
   };
 
   return (
@@ -90,6 +144,54 @@ export const ProductEdit = ({ product, categories }: { product: Product; categor
                 ))}
               </select>
             </label>
+          </div>
+
+          <div className={styles["form-group"]}>
+            <label htmlFor="ingredient" className={styles["form-label"]}>
+              食材
+              <button
+                type="button"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setOpenIngredientModal(true);
+                }}
+                className={styles["form-button"]}
+              >
+                食材を選択
+              </button>
+              <div>選択された食材: {selectedIngredients.join(", ")}</div>
+            </label>
+            {openIngredientModal && (
+              <div className={styles["modal"]} onClick={handleModalOutsideClick}>
+                <div className={styles["modal-content"]} onClick={handleModalInsideClick}>
+                  {ingredients.map((item) => (
+                    <button
+                      type="button"
+                      key={item.ingredientId}
+                      onClick={() => toggleIngredientSelection(item.ingredientId)}
+                      className={
+                        ingredient.includes(item.ingredientId) ? styles["modal-button-active"] : styles["modal-button"]
+                      }
+                    >
+                      {item.ingredientName}
+                    </button>
+                  ))}
+                </div>
+                <div className={styles["preview-selected-item"]}>
+                  <div className={styles["preview-selected-item-title"]}>選択された食材</div>
+                  <button
+                    type="button"
+                    className={styles["clear-selected-item"]}
+                    onClick={() => clearSelectedItems("ingredient")}
+                  >
+                    すべて解除する
+                  </button>
+                  <div className={styles["preview-selected-item-content"]}>
+                    {selectedIngredients.length > 0 ? selectedIngredients.join(", ") : "なし"}
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </form>
       </div>
