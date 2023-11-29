b5b986248759e982cc2af136623d675dffce740e
yuto <yuto154110@gmail.com>
2023-11-29 11:39:08 +0900

    いらないとこの削除

--- src/features/User/AccountDeactivate/Confirm/doConfirm.ts
+++ src/features/User/AccountDeactivate/Confirm/doConfirm.ts
@@ -1,44 +1,28 @@
 import { getAuth, deleteUser, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
 import { firebaseApp } from "@/utils/Firebase/firebaseConfig";
-import { prisma } from "@/utils/Prisma/PrismaClient";
 
 export const doConfirm = async (password: string): Promise<boolean> => {
-  const auth = getAuth(firebaseApp);
-  const user = auth.currentUser;
-
-  if (!user || !user.email) {
-    throw new Error("ユーザーが存在しません。");
-  }
-  const credential = EmailAuthProvider.credential(user.email, password);
-
   try {
+    const auth = getAuth(firebaseApp);
+    const user = auth.currentUser;
+
+    if (!user || !user.email) {
+      throw new Error("ユーザーが存在しません。");
+    }
+
+    const credential = EmailAuthProvider.credential(user.email, password);
+
     // ユーザーを再認証
     await reauthenticateWithCredential(user, credential);
 
     // ユーザーアカウントを削除
     await deleteUser(user);
 
-    // Firebaseでの削除が完了した後、データベースの更新処理を実行
-    try {
-      const updatedUser = await prisma.user.update({
-        where: {
-          userId: user.id, // ユーザーのIDを指定
-        },
-        data: {
-          isDeleted: true,
-          deletedAt: new Date(), // 削除した時刻を設定
-        },
-      });
-
-      return true; // 削除成功を示すtrueを返す
-    } catch (e) {
-      console.error(e);
-      return false; // 削除失敗を示すfalseを返す
-    } finally {
-      await prisma.$disconnect();
-    }
+    // 削除成功を示すtrueを返す
+    return true;
   } catch (error) {
     console.error(error);
-    return false; // 削除失敗を示すfalseを返す
+    // 削除失敗を示すfalseを返す
+    return false;
   }
 };
