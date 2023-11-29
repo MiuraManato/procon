39e2a8caa96f2fd51a0fb354498268b5b2b6ab34
yuto <yuto154110@gmail.com>
2023-11-29 11:47:08 +0900

    feat エラー処理の追加

--- src/pages/api/auth/confirm/index.ts
+++ src/pages/api/auth/confirm/index.ts
@@ -2,24 +2,29 @@ import { prisma } from "@/utils/Prisma/PrismaClient";
 import { NextApiResponse } from "next";
 import { NextApiRequestExtendsUser } from "@/types/api/signup";
 
-const signupHandler = async (req: NextApiRequestExtendsUser, res: NextApiResponse) => {
+const confirmHandler = async (req: NextApiRequestExtendsUser, res: NextApiResponse) => {
   const { uid } = req.body;
   if (!uid) {
     return res.status(400).json({ message: "Invalid request" });
   }
 
-
-   // Firebaseでの削除が完了した後、データベースの更新処理を実行
+  // Firebaseでの削除が完了した後、データベースの更新処理を実行
   try {
-  const updatedUser = await prisma.user.update({
-    where: {
-      userId: user.id, // ユーザーのIDを指定
-    },
-    data: {
-      isDeleted: true,
-      deletedAt: new Date(), // 削除した時刻を設定
-    },
-  });
+    const updatedUser = await prisma.user.update({
+      where: {
+        userId: user.id, // ユーザーのIDを指定
+      },
+      data: {
+        isDeleted: true,
+        deletedAt: new Date(), // 削除した時刻を設定
+      },
+    });
+    res.status(200).json({ message: "User updated successfully", user: updatedUser });
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong. Please try again later." });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
 
-
-export default signupHandler;
+export default confirmHandler;
