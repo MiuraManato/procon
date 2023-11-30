6cb5ce6db58dd0f44c12a60a751b030c3c087e0d
yuto <yuto154110@gmail.com>
2023-11-30 11:49:03 +0900

    コメントの追加

--- src/features/User/AccountDeactivate/Confirm/doConfirm.ts
+++ src/features/User/AccountDeactivate/Confirm/doConfirm.ts
@@ -1,18 +1,27 @@
+// Firebase Authenticationから必要なメソッドをインポート
 import { getAuth, deleteUser, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
+
+// Firebaseアプリの設定をインポート
 import { firebaseApp } from "@/utils/Firebase/firebaseConfig";
 
+// ユーザーアカウントの削除を行う関数
 export const doConfirm = async (password: string): Promise<boolean> => {
   try {
+    // Firebase Authenticationからauthオブジェクトを取得
     const auth = getAuth(firebaseApp);
+
+    // 現在ログインしているユーザー情報を取得
     const user = auth.currentUser;
 
+    // もしユーザーが存在しないか、メールアドレスが存在しない場合はエラーをスロー
     if (!user || !user.email) {
       throw new Error("ユーザーが存在しません。");
     }
 
+    // ユーザーのメールアドレスとパスワード情報から認証資格情報を取得
     const credential = EmailAuthProvider.credential(user.email, password);
 
-    // ユーザーを再認証
+    // ユーザーを再認証し、認証情報が正しいか確認
     await reauthenticateWithCredential(user, credential);
 
     // ユーザーアカウントを削除
--- src/pages/api/auth/confirm/index.ts
+++ src/pages/api/auth/confirm/index.ts
@@ -1,28 +1,40 @@
+// Prismaのインスタンスをimport
 import { prisma } from "@/utils/Prisma/PrismaClient";
+
+// Next.jsのAPIレスポンスとリクエストをimport
 import { NextApiResponse } from "next";
 import { NextApiRequestExtendsUser } from "@/types/api/signup";
 
+// ユーザー削除リクエストのハンドラー関数
 const confirmHandler = async (req: NextApiRequestExtendsUser, res: NextApiResponse) => {
+  // リクエストからユーザーIDを取得
   const { uid } = req.body;
+
+  // ユーザーIDが無効な場合はエラーを返す
   if (!uid) {
     return res.status(400).json({ message: "Invalid request" });
   }
 
   // Firebaseでの削除が完了した後、データベースの更新処理を実行
   try {
+    // Prismaを使用してユーザー情報を更新
     const updatedUser = await prisma.user.update({
       where: {
-        userId: uid, // ユーザーのIDを指定
+        userId: uid, // 削除対象のユーザーのIDを指定
       },
       data: {
-        isDeleted: true,
+        isDeleted: true, // ユーザーを削除済みにマーク
         deletedAt: new Date(), // 削除した時刻を設定
       },
     });
+
+    // 更新が成功した場合は成功レスポンスを返す
     res.status(200).json({ message: "User updated successfully", user: updatedUser });
   } catch (e) {
+    // エラーが発生した場合はエラーレスポンスを返す
     res.status(500).json({ message: "Something went wrong. Please try again later." });
   } finally {
+    // Prismaとの接続を切断
     await prisma.$disconnect();
   }
 };
