1caec199dbb2a7adc297a33494cb8e73d5638ae4
manato miura <mmiura2351@gmail.com>
2023-12-01 11:42:26 +0900

    feat: メール認証がされていない場合にログインできないように修正

--- src/features/User/Authentication/Login/doLogin.ts
+++ src/features/User/Authentication/Login/doLogin.ts
@@ -1,11 +1,21 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { firebaseApp } from "@/utils/Firebase/firebaseConfig";
 
 export const doLogin = async (email: string, password: string): Promise<boolean> => {
   const auth = getAuth(firebaseApp);
   try {
-    await signInWithEmailAndPassword(auth, email, password);
-    return true;
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    // メール認証が完了しているか確認
+    if (user.emailVerified) {
+      return true;
+    } else {
+      // メール認証がされていないため、ログアウト処理を行う
+      await signOut(auth);
+      console.error("メール認証が完了していません。");
+      return false;
+    }
   } catch (e) {
     console.error(e);
     return false;
