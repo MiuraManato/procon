7f4c98fb71ab14c112f7a13684618eda9f82dd72
MiuraManato <manato20030510@icloud.com>
2023-12-07 14:30:20 +0900

    feat: 戻るボタンを追加

--- src/features/Order/Employee/Settings/index.tsx
+++ src/features/Order/Employee/Settings/index.tsx
@@ -10,6 +10,10 @@ const toOrderEmployeeLogin = () => {
   router.push("/order/employee/login").catch((err) => console.error(err));
 };
 
+const toOrderTop = () => {
+  router.push("/order").catch((err) => console.error(err));
+};
+
 export const OrderEmployeeSettings = () => {
   const user = useAuth();
   if (user === null) {
@@ -20,6 +24,13 @@ export const OrderEmployeeSettings = () => {
       <div className={styles.container}>
         <div className={styles.title}>
           <h1>設定画面</h1>
+        </div>
+        <div className={styles["items"]}>
+          <div className={styles["item"]}>
+            <button className={styles["item-button"]} onClick={toOrderTop}>
+              <span>人数登録画面</span>
+            </button>
+          </div>
         </div>
         <div className={styles["items"]}>
           <div className={styles["item"]}>
