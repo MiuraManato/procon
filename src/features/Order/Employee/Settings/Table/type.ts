c50026e5a43d341d66a02fe9b3d7ace8d9cc9f4a
MiuraManato <manato20030510@icloud.com>
2023-12-07 21:20:23 +0900

    clean: 使わなくなった型を消した

--- src/features/Order/Employee/Settings/Table/type.ts
+++ src/features/Order/Employee/Settings/Table/type.ts
@@ -1,12 +1,3 @@
-export interface storeTable {
-  tableId: number;
-  tableName: string;
-  store: {
-    storeId: number;
-    storeName: string;
-  };
-}
-
 export interface tables {
   storeId: number;
   storeName: string;
