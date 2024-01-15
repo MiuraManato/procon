bff1eb212473c3f41055daf54350f4fa34515d42
yuto <yuto154110@gmail.com>
2024-01-15 11:55:02 +0900

    style作成時の画面調整

--- src/features/User/Authentication/Signup/index.tsx
+++ src/features/User/Authentication/Signup/index.tsx
@@ -110,113 +110,150 @@ export const Signup = () => {
       <Head>
         <title>ユーザー登録</title>
       </Head>
-      <form method={"post"} onSubmit={handleSubmit}>
-        <label>
-          ユーザー名
-          <input
-            type="text"
-            value={username}
-            onBlur={() => handleBlur("username")}
-            onChange={(e) => handleUsernameChange(e.target.value)}
-          />
-          {touched.username && !username && <span>ユーザー名を入力してください</span>}
-        </label>
-        <br />
-        <label>
-          名前
-          <input
-            type="text"
-            value={lastName}
-            onBlur={() => handleBlur("lastName")}
-            onChange={(e) => handleLastNameChange(e.target.value)}
-          />
-          <input
-            type="text"
-            value={firstName}
-            onBlur={() => handleBlur("firstName")}
-            onChange={(e) => handleFirstNameChange(e.target.value)}
-          />
-          {touched.firstName && !firstName && touched.lastName && !lastName && <span>名前を入力してください</span>}
-        </label>
-        <br />
-        <label>
-          年齢
-          <input
-            type="number"
-            value={age}
-            onBlur={() => handleBlur("age")}
-            onChange={(e) => handleAgeChange(Number(e.target.value))}
-          />
-          {touched.age && !age && <span>年齢を入力してください</span>}
-        </label>
-        <br />
-        <label>
-          メールアドレス
-          <input
-            type="email"
-            value={email}
-            onBlur={() => handleBlur("email")}
-            onChange={(e) => handleEmailChange(e.target.value)}
-          />
-          {touched.email && email && !ValidateEmail(email) && <span>メールアドレスの形式が正しくありません</span>}
-        </label>
-        <br />
-        <label>
-          パスワード
-          <input
-            type={showPassword ? "text" : "password"}
-            value={password}
-            onBlur={() => handleBlur("password")}
-            onChange={(e) => handlePasswordChange(e.target.value)}
-          />
-          <button type="button" onClick={togglePasswordVisibility}>
-            <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
-          </button>
-          {touched.password && password && !ValidatePassword(password) && (
-            <span>パスワードは小文字、大文字、数字を含む8文字以上にする必要があります。</span>
-          )}
-        </label>
-        <br />
-        <label>
-          パスワード再入力
-          <input
-            type="password"
-            value={passwordConfirmation}
-            onBlur={() => handleBlur("passwordConfirmation")}
-            onChange={(e) => handlePasswordConfirmation(e.target.value)}
-          />
-          {touched.passwordConfirmation &&
-            passwordConfirmation &&
-            !CheckPasswordMatch(password, passwordConfirmation) && <span>パスワードが一致しません</span>}
-        </label>
-        <br />
-        <button
-          type="submit"
-          disabled={
-            !username ||
-            !email ||
-            !password ||
-            !passwordConfirmation ||
-            !ValidateEmail(email) ||
-            !ValidatePassword(password) ||
-            !CheckPasswordMatch(password, passwordConfirmation)
-          }
-        >
-          登録
-        </button>
-      </form>
-      <div>
-        <Link href={"/user/auth/login"}>ログインはこちら</Link>
-      </div>
-      {modalIsOpen && (
-        <div className={`${styles["outside-modal"]}`}>
-          <div className={`${styles["confirm-modal"]}`}>
-            <p>ユーザー登録が完了しました。</p>
-            <p>メールアドレス認証をした後、ログインをしてください。</p>
-            <Link href={"/user/auth/login"}>ログインはこちら</Link>
+      <div className={styles.body}>
+        <form method="post" onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.inputGroup}>
+            <label className={styles.label}>
+              <div className={styles["label-text"]}>ユーザー名</div>
+              <div className={styles["input-box"]}>
+                <input
+                  type="text"
+                  value={username}
+                  onBlur={() => handleBlur("username")}
+                  onChange={(e) => handleUsernameChange(e.target.value)}
+                />
+              </div>
+              {touched.username && !username && <span className={styles.span}>ユーザー名を入力してください</span>}
+            </label>
           </div>
-        </div>
-      )}
+          <div className={styles.inputGroup}>
+            <label className={styles.label}>
+              <div className={styles["label-text"]}>姓</div>
+              <div className={styles["input-box"]}>
+                <input
+                  type="text"
+                  value={lastName}
+                  onBlur={() => handleBlur("lastName")}
+                  onChange={(e) => handleLastNameChange(e.target.value)}
+                />
+              </div>
+              <div className={styles["label-text"]}>名</div>
+              <div className={styles["input-box"]}>
+                <input
+                  type="text"
+                  value={firstName}
+                  onBlur={() => handleBlur("firstName")}
+                  onChange={(e) => handleFirstNameChange(e.target.value)}
+                />
+              </div>
+              {touched.firstName && !firstName && touched.lastName && !lastName && (
+                <span className={styles.span}>名前を入力してください</span>
+              )}
+            </label>
+          </div>
+          <div className={styles.inputGroup}>
+            <label className={styles.label}>
+              <div className={styles["label-text"]}>年齢</div>
+              <div className={styles["input-box"]}>
+                <input
+                  type="number"
+                  value={age}
+                  onBlur={() => handleBlur("age")}
+                  onChange={(e) => handleAgeChange(Number(e.target.value))}
+                />
+              </div>
+              {touched.age && !age && <span className={styles.span}>年齢を入力してください</span>}
+            </label>
+          </div>
+          <div className={styles.inputGroup}>
+            <label className={styles.label}>
+              <div className={styles["label-text"]}>メールアドレス</div>
+              <div className={styles["input-box"]}>
+                <input
+                  type="email"
+                  value={email}
+                  onBlur={() => handleBlur("email")}
+                  onChange={(e) => handleEmailChange(e.target.value)}
+                />
+              </div>
+              {touched.email && email && !ValidateEmail(email) && (
+                <span className={styles.span}>メールアドレスの形式が正しくありません</span>
+              )}
+            </label>
+          </div>
+          <div className={styles.inputGroup}>
+            <label className={styles.label}>
+              <div className={styles["label-text"]}>パスワード</div>
+              <div className={styles["input-box"]}>
+                <input
+                  className={styles["form-input"]}
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onBlur={() => handleBlur("password")}
+                  onChange={(e) => handlePasswordChange(e.target.value)}
+                />
+                <button type="button" onClick={togglePasswordVisibility}>
+                  <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+                </button>
+              </div>
+              {touched.password && password && !ValidatePassword(password) && (
+                <span className={styles.span} style={{ whiteSpace: "pre-line" }}>
+                  パスワードは小文字、大文字、数字を含む8文字以上にする必要があります。
+                </span>
+              )}
+            </label>
+          </div>
+          <div className={styles.inputGroup}>
+            <label className={styles.label}>
+              <div className={styles["label-text"]}>パスワード再入力</div>
+              <div className={styles["input-box"]}>
+                <input
+                  type="password"
+                  value={passwordConfirmation}
+                  onBlur={() => handleBlur("passwordConfirmation")}
+                  onChange={(e) => handlePasswordConfirmation(e.target.value)}
+                />
+              </div>
+              {touched.passwordConfirmation &&
+                passwordConfirmation &&
+                !CheckPasswordMatch(password, passwordConfirmation) && (
+                  <span className={styles.span}>パスワードが一致しません</span>
+                )}
+            </label>
+          </div>
+          <button
+            type="submit"
+            disabled={
+              !username ||
+              !email ||
+              !password ||
+              !passwordConfirmation ||
+              !ValidateEmail(email) ||
+              !ValidatePassword(password) ||
+              !CheckPasswordMatch(password, passwordConfirmation)
+            }
+            className={styles.button}
+          >
+            登録
+          </button>
+          <div className={styles.linkContainer}>
+            <Link href={"/user/auth/login"} className={styles.link}>
+              ログインはこちら
+            </Link>
+          </div>
+        </form>
+        {modalIsOpen && (
+          <div className={styles["outside-modal"]}>
+            <div className={styles["confirm-modal"]}>
+              <p>ユーザー登録が完了しました。</p>
+              <p>メールアドレス認証をした後、ログインをしてください。</p>
+              <Link href={"/user/auth/login"} className={styles.link}>
+                ログインはこちら
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
