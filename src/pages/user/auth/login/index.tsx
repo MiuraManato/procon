c5583016a82aa8c415eb0f14665c6f95787e75e2
yuto <yuto154110@gmail.com>
2024-01-18 11:17:04 +0900

    ヘッダーの追加

--- src/pages/user/auth/login/index.tsx
+++ src/pages/user/auth/login/index.tsx
@@ -1,7 +1,12 @@
+import { UserHeader } from "@/components/User/Header";
 import { Login } from "@/features/User/Authentication/Login";
 
 const LoginPage = () => {
-  return <Login />;
+  return
+  <>
+  <EmployeeHeader />
+  <Login />;
+  </>
 };
 
 export default LoginPage;
