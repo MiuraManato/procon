d7040749f2a8fcf34188787ec19e31e08e7d8771
yuto <yuto154110@gmail.com>
2024-01-18 11:25:38 +0900

    ヘッダーの削除

--- src/pages/user/auth/login/index.tsx
+++ src/pages/user/auth/login/index.tsx
@@ -1,13 +1,7 @@
-import { UserHeader } from "@/components/User/Header";
 import { Login } from "@/features/User/Authentication/Login";
 
 const LoginPage = () => {
-  return(
-    <div>
-      <UserHeader />
-      <Login />
-    </div>
-  )
+  return <Login />;
 };
 
 export default LoginPage;
