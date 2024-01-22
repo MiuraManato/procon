063dc29855e037b956d0e25d476efe53dba77d3c
yuto <yuto154110@gmail.com>
2024-01-22 10:10:38 +0900

    パスワードバリデーションの修正

--- src/utils/Auth/ValidatePassword/index.ts
+++ src/utils/Auth/ValidatePassword/index.ts
@@ -1,6 +1,6 @@
 const ValidatePassword = (password: string): boolean => {
   // かっこ系の記号\は使えません
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9[ -/:-@[-`{-~]]{8,}$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9!-/:-@[-`{-~]{8,}$/;
   return passwordRegex.test(password);
 };
 
