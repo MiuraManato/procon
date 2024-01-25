424b628f3e077e12aded014458b31be29b8cb92f
manato miura <mmiura2351@gmail.com>
2024-01-25 11:58:46 +0900

    feat: アレルギーの一覧を取得する関数を作成

---
+++ src/features/User/Settings/Allergy/getAllergies.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/utils/Prisma/PrismaClient";
+
+export const getAllergies = async () => {
+  const allergies = await prisma.allergy.findMany({
+    select: {
+      allergyId: true,
+      allergyName: true,
+    },
+  });
+
+  return { allergies };
+};
