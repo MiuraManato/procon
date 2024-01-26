deeb1cb88002c2bc13a813a123c7449e8e91017f
yuto <yuto154110@gmail.com>
2024-01-26 11:18:40 +0900

    退会、パスワード設定、プロフィール設定

--- src/pages/user/account/settings/change-password/index.tsx
+++ src/pages/user/account/settings/change-password/index.tsx
@@ -1,7 +1,13 @@
 //パスワード変更画面
 //制作者：吉澤
 import { ChangePassword } from "@/features/User/AccountSettings/ChangePassword";
+import { UserHeader } from "@/components/User/Header";
 
 export default function ChangePasswordPage() {
-  return <ChangePassword />;
+  return (
+    <div>
+      <UserHeader />
+      <ChangePassword />
+    </div>
+  );
 }
