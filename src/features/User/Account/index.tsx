16c09dd554e766d9ce32b1c3347125795de58bff
manato miura <mmiura2351@gmail.com>
2024-02-02 11:07:49 +0900

    feat: ログアウトボタンを追加

--- src/features/User/Account/index.tsx
+++ src/features/User/Account/index.tsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 import styles from "./index.module.css";
 import useAuth from "@/features/hooks/useAuth";
 import { useEffect, useState } from "react";
+import { firebaseApp } from "@/utils/Firebase/firebaseConfig";
+import { getAuth } from "firebase/auth";
+import router from "next/router";
 
 export const AccountInfo = () => {
   const [loading, setLoading] = useState<boolean>(true);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
   const user = useAuth();
+  const auth = getAuth(firebaseApp);
+
   useEffect(() => {
     if (user === undefined) return;
     if (user === null) {
@@ -17,7 +23,24 @@ export const AccountInfo = () => {
     setLoading(false);
   }, [user]);
 
-  if (loading) return <>Now loading...</>;
+  const doLogout = async () => {
+    try {
+      await auth.signOut();
+      await router.push("/user/auth/login");
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+
+  const closeModal = () => {
+    setIsLogoutModalOpen(false);
+  };
+
+  const stopPropagation = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  if (loading) return <>Loading...</>;
 
   return (
     <>
@@ -32,10 +55,42 @@ export const AccountInfo = () => {
         <Link href={"/user/account/settings/change-password"} className={styles.link}>
           <div className={styles.btn}>パスワード変更</div>
         </Link>
+        <button className={styles.btn} onClick={() => setIsLogoutModalOpen(true)}>
+          ログアウト
+        </button>
         <Link href={"/user/account/deactivate/confirm"} className={styles.link}>
-          <div className={styles.btn}>退会</div>
+          <div className={styles.tbtn}>退会</div>
         </Link>
       </div>
+      {isLogoutModalOpen && (
+        <div className={styles["logout-modal-background"]} onClick={closeModal}>
+          <div className={styles["logout-modal"]} onClick={stopPropagation}>
+            <div className={styles["logout-modal-container"]}>
+              <div className={styles["logout-modal-title"]}>ログアウトしますか？</div>
+              <div className={styles["logout-modal-button-container"]}>
+                <button
+                  type="button"
+                  className={styles["logout-modal-button"]}
+                  onClick={() => {
+                    setIsLogoutModalOpen(false);
+                  }}
+                >
+                  キャンセル
+                </button>
+                <button
+                  type="button"
+                  className={styles["logout-modal-logout-button"]}
+                  onClick={() => {
+                    void doLogout();
+                  }}
+                >
+                  ログアウト
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
